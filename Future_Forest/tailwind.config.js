6331a707f1bcd1b8dbd9c238ba74a745f6989645
sasithhansaka <sasithhansaka285@gmail.com>
2025-08-19 11:12:20 +0530

    Refactor tailwind.config.js for improved formatting and readability

--- Future_Forest/tailwind.config.js
+++ Future_Forest/tailwind.config.js
@@ -1,28 +1,29 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-    './storage/framework/views/*.php',
-    './resources/views/**/*.blade.php',
-    './resources/js/**/*.{js,jsx,ts,tsx}',
-  ],
+    content: [
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.{js,jsx,ts,tsx}",
+    ],
 
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-      },
-      fontSize: {
-        '10xl': '10rem',
-        '11xl': '12rem',
-        '12xl': '14rem',
-        '13xl': '16rem',
-      },
+    theme: {
+        extend: {
+            fontFamily: {
+                // sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+                poppins: ["Poppins", "sans-serif"], // add Poppins
+            },
+            fontSize: {
+                "10xl": "10rem",
+                "11xl": "12rem",
+                "12xl": "14rem",
+                "13xl": "16rem",
+            },
+        },
     },
-  },
 
-  plugins: [forms],
+    plugins: [forms],
 };
